218b4a5810e4b80f803e9e92282e062894e0c301
deepakprasad <official.deepakprasad@gmail.com>
2025-02-08 10:45:28 +0530

    Character Pattern

---
+++ pattern4.java
@@ -0,0 +1,26 @@
+// n = 4
+// A
+// BC
+// CDE
+// DEFG
+
+import java.util.Scanner;
+
+public class pattern4 {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            
+            for(int i=0; i<n ;i++) {
+                for(int j=0; j<=i ; j++) {
+                    System.out.print((char)('A'+i+j)); //('E'-j)
+                }
+                System.out.println();
+            }
+    
+        }
+    
+}
+    
+
