341ed2e757cead68fef1d3433852215daeb5863d
deepakprasad <official.deepakprasad@gmail.com>
2025-02-14 10:41:50 +0530

    Numeric Pattern
    Asked in Different exam.

---
+++ pattern9.java
@@ -0,0 +1,29 @@
+
+// n = 4
+// 432*
+// 43*1
+// 4*21
+// *321
+
+import java.util.*;
+
+public class test_pattern {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for(int i =1 ; i<=n ; i++){
+	      for(int j =n ; j>=1 ; j-- ){
+			  if(j==i){
+				  System.out.print("*");
+			  }
+			  else{
+				  System.out.print(j);
+			  }
+		  }
+		  System.out.println();
+		}	
+
+		}
+	}
