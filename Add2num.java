d17a7a4b8e035f2baebca8d7b055eb60928bd3a9
deepakprasad <official.deepakprasad@gmail.com>
2025-02-14 10:50:00 +0530

    Add 2 number

---
+++ Add2num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Add2num {
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int i = sc.nextInt();
+		System.out.println("Enter second number: ");
+		int j = sc.nextInt();
+         int sum = i + j;
+         System.out.println(sum);
+	}
+
+}
+    
