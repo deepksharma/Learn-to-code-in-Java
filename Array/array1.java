1c344209c16a33bdf81aba215f7ee1979c3d8f92
deepakprasad <official.deepakprasad@gmail.com>
2025-02-19 11:13:08 +0530

    how to take input in array and print it

---
+++ Array/array1.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+public class array1 {
+
+	public static void main(String[] args) {
+     
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int input[] = new int[size];
+		
+		for(int i =0 ; i<size ; i++) {
+			input[i] = sc.nextInt();
+		}
+		for(int i =0 ; i<size ; i++) {
+			System.out.print(input[i]+" ");
+		}
+		
+	}
+}
+
+// how to take input in array and print it
