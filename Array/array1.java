38d5e682df9f8e54e9f5e22d71260859f93a340d
deepakprasad <official.deepakprasad@gmail.com>
2025-02-19 11:30:30 +0530

    Update array1.java

--- Array/array1.java
+++ Array/array1.java
@@ -6,8 +6,10 @@ public class array1 {
 	public static void main(String[] args) {
      
 		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
 		int size = sc.nextInt();
 		int input[] = new int[size];
+        System.out.println("Enter the number you want to insert: ");
 		
 		for(int i =0 ; i<size ; i++) {
 			input[i] = sc.nextInt();
